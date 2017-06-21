eef9eeac7cd2a89d5596654baf6193a0c691afc9
Brotat0 <brotatoluvspotatoes@gmail.com>
2017-06-21 22:22:21 +0300

    For Tampermonkey users

---
+++ Userscript/dubtrackneon.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         [CSS] Dionysis - Dubtrack Neon
+// @namespace    CSS
+// @version      0.1
+// @description  -
+// @author       Dionysis
+// @match        https://*.dubtrack.fm/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var cssUrl = 'https://brotat0.github.io/CSS/dubtrackneon.css';
+
+
+    $(document).ready(function () {
+        $('head').append('<link href="' + cssUrl + '" rel="stylesheet" type="text/css">');
+    });
+})();
+
+
