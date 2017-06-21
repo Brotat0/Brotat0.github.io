2df944e097e1e52cc4ffa4a0525608885e335f35
Brotat0 <brotatoluvspotatoes@gmail.com>
2017-06-21 22:24:29 +0300

    For Tampermonkey users

---
+++ Userscript/mediumpurple.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         [CSS] Dionysis - Mediumpurple
+// @namespace    CSS
+// @version      0.1
+// @description  -
+// @author       Dionysis
+// @match        https://*.dubtrack.fm/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var cssUrl = 'https://brotat0.github.io/CSS/mediumpurple.css';
+
+
+    $(document).ready(function () {
+        $('head').append('<link href="' + cssUrl + '" rel="stylesheet" type="text/css">');
+    });
+})();
