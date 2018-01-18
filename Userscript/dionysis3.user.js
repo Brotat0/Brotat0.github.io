ce9dd5bb3bac17687b1adc298ca469261c278c16
Dionysis <brotatoluvspotatoes@gmail.com>
2018-01-18 22:28:38 +0200

    For Tampermonkey Users

---
+++ Userscript/dionysis3.user.js
@@ -0,0 +1,21 @@
+s (16 sloc)  463 Bytes
+// ==UserScript==
+// @name         [CSS] Dionysis #3
+// @namespace    CSS
+// @version      0.1
+// @description  -
+// @author       Dionysis
+// @match        https://*.dubtrack.fm/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var cssUrl = 'https://brotat0.github.io/CSS/dionysis3.css';
+
+
+    $(document).ready(function () {
+        $('head').append('<link href="' + cssUrl + '" rel="stylesheet" type="text/css">');
+    });
+})();
